3c8dc0006ae8ec437aa4a880d145fd6889925834
Pietro Dessotti <pietrohdessotti@gmail.com>
2021-04-22 17:36:36 -0300

    edit javascript

--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
-var dolar = 5.71;
-var dolarCanadense = 4.54;
-var euro = 6.71;
-var libra = 7.89;
-var pesoArgentino = 0.062;
+const dolar = 5.71;
+const dolarCanadense = 4.54;
+const euro = 6.71;
+const libra = 7.89;
+const pesoArgentino = 0.062;
+
+function createInput() {
+    const html = `
+    
+    <h1 class="page-title">Conversor de moedas</h1>
+
+    <select name="Moeda" id="moeda" onClick="moeda">
+                <option value="0">Selecione uma Moeda</option>
+                <option value="1">Dólar</option>
+                <option value="2">Dólar Canadense</option>
+                <option value="3">Euro</option>
+                <option value="4">Libra</option>
+                <option value="5">Peso Argentino</option>
+    </select>
+
+    <input type="text" id="conversao" placeholder="Digite um valor" oncick="converter()">
+    <input type="text" id="convertido"></input>
+
+    <div class="buttonConvert">
+    <button onclick="convert()">Converter</button>`
+
+
+    const conversor = document.getElementById('conversor')
+    conversor.innerHTML = html;
+
+}
+
+createInput()
 
 function convert() {
     var moeda = $("#moeda").val();
     var conversao = document.getElementById("conversao").value
     var valorReal = parseFloat(conversao)
-    
+
     if (moeda == 0) {
         convertido.value = "Selecione a moeda de cambio!";
 
